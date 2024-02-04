b57460745783ff22dd7b028da72b843e40fb5d33
Богдан Грибачев <banlypro@gmail.com>
2024-02-04 17:38:37 +0300

    ggzelenskiylohhohlipidoriputinfrutZStopwarVOchko

--- Tree/Tree.h
+++ Tree/Tree.h
@@ -4,7 +4,33 @@ template <typename T>
 class Tree {
 public:
 	Tree() {
+		root = nullptr;
+	}
+	void add(T data) {
+		if (!root) root = new Node(data);
+		else add(root, data);
+	}
+private:
+	void add(Node* current, T data)
+	{
+		if (data == current->data) return;
 		
+		if (data < current->data) {
+			if (current->left) add(current->left, data);
+			else
+			{
+				current->left = new Node(data);
+				current->left->parent = current;
+			}
+		}
+		else
+		if (data > current->data) {
+			if (current->right) add(current->right, data);
+			else {
+				current->right = new Node(data);
+				current->right->data = current;
+			}
+		}
 	}
 private:
 	class Node {
